2b21ba1da312eda425407530a84fa729fc2236d6
Kaylee Rosendahl <98189787+kjrosendahl@users.noreply.github.com>
2023-02-07 01:06:21 -0600

    auto crawl

--- user-crawl.py
+++ user-crawl.py
@@ -26,65 +26,71 @@ api = tweepy.API(auth, wait_on_rate_limit=True)
 user_count = 0 
 user_dict = dict()
 queue = [] 
-max_users = 5
-max_percent = .01
+max_users = 30
+max_percent = .02
 
-init = api.get_user(screen_name = "michaelee")
+init = 8023702
+
+# init = api.get_user(screen_name = "michaelee")
+
+# print(init.id)
 
 queue.append(init)
-# queue.append("michaelee")
 
 while queue and user_count < max_users: 
 
     start_user = queue.pop(0)
-    # start_user_name = queue.pop(0)
-    # start_user = api.get_user(screen_name=start_user_name)
+    print(start_user)
+    followers = api.get_follower_ids(user_id = start_user)
+    friends = api.get_friend_ids(user_id = start_user)
+    # followers.sort() 
+    # friends.sort() 
 
-    max_friends = np.ceil(max_percent*start_user.friends_count)
-    max_following = np.ceil(max_percent*start_user.followers_count)
+    max_following = np.ceil(max_percent*len(followers))
+    max_friends = np.ceil(max_percent*len(friends))
+
+    print(len(followers), len(friends), max_following, max_friends)
+
+    user_following_count_taken = 0
     user_friend_count_taken = 0 
-    user_following_count_taken = 0 
 
-    for friend in start_user.friends():
+    for fol in followers: 
+
+        follower_obj = api.get_user(user_id = fol)
+        if (follower_obj.friends_count > 200) or (follower_obj.followers_count > 200): 
+            continue 
+
+        if fol in user_dict: 
+            user_dict[fol][1].append(start_user) 
+        else: 
+            user_dict[fol] = ([], [start_user])
+            queue.append(fol)
+            user_count += 1
         
-        if (friend.friends_count < 500) and (friend.followers_count < 500): 
-
-            # if already discovered user 
-            if friend in user_dict: 
-                user_dict[friend][0].append(start_user)
-
-            # add to dictionary 
-            else: 
-                user_dict[friend] = ([start_user], [])
-                queue.append(friend)
-                user_count += 1 
-
-            user_friend_count_taken += 1
-            
-        if user_friend_count_taken > max_friends: 
-            break
- 
-    for follower in start_user.followers(): 
-
-        if (follower.friends_count < 500) and (follower.followers_count < 500): 
-
-            if follower in user_dict: 
-                user_dict[follower][1].append(start_user)
-            else: 
-                user_dict[follower] = ([], [start_user])
-                queue.append(follower)
-                user_count += 1 
-
-            user_following_count_taken += 1 
-
+        user_following_count_taken += 1
         if user_following_count_taken > max_following: 
-            break
+            break 
  
+    for fr in friends: 
+
+        friend_obj = api.get_user(user_id = fr)
+        if (friend_obj.friends_count > 200) or (friend_obj.followers_count > 200): 
+            continue 
+        
+        if fr in user_dict: 
+            user_dict[fr][0].append(start_user)
+        else: 
+            user_dict[fr] = ([start_user], [])
+            queue.append(fr)
+            user_count += 1
+        
+        user_friend_count_taken += 1
+        if user_friend_count_taken > max_friends: 
+            break 
 
 # for user in queue: 
 #     print(user.screen_name)
-
-# for k, v in user_dict.items(): 
-#     print(k, v)
-
-print(len(user_dict))
+print(user_count)
+with open('users.txt', 'w') as f:
+    for key, value in user_dict.items(): 
+        f.write('%s %s\n' %(key, value))
